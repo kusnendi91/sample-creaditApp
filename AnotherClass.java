74d550d2c744cdaeffa057f0f1fbb32b0270fd52
kusnendi <nendikus@gmail.com>
2017-10-02 14:00:47 +0700

    change all project from sample project to actual project

--- AnotherClass.java
+++
@@ -1,5 +0,0 @@
-public class AnotherClass{
-	public static void main(String[] args){
-		System.out.println("This is another class");
-	}
-}
--- Sample_Class.java
+++
@@ -1,5 +0,0 @@
-public class Sample_Class{
-    public static void main(String ,args[]){
-    	System.out,print("Hello");
-	}
-}
